9dd9f7fe5b3752eb42800329a28f41b89d345cfa
Soochin Kang <soochink@uci.edu>
2020-08-15 13:47:00 -0700

    Initializing repo with ome page, container, routes, sign up, and csrf implemented

---
+++ app/Validation/Exceptions/EmailAvailableException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Validation\Exceptions;
+
+use Respect\Validation\Exceptions\ValidationException;
+
+
+final class EmailAvailableException extends ValidationException {
+    protected $defaultTemplates = [
+        self::MODE_DEFAULT => [
+            self::STANDARD => 'Email is already in use.',
+        ],
+    ];
+}
+
+?>
